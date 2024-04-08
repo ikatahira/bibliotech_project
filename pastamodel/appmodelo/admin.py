bdd12d380a876d8cc0a8d2422f85acca04adedcf
ikatahira <isaque.katahira@fatec.sp.gov.br>
2024-04-08 17:42:57 +0000

    Genero, Editora, Autor e Livro do projeto Bibliotech

--- pastamodel/appmodelo/admin.py
+++ pastamodel/appmodelo/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Genero, Editora, Autor, Livro
 # Register your models here.
+admin.site.register(Genero)
+admin.site.register(Editora)
+admin.site.register(Autor)
+admin.site.register(Livro)
--- pastamodel/appmodelo/models.py
+++ pastamodel/appmodelo/models.py
@@ -1,5 +1,35 @@
 from django.db import models
 
+class Genero(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+class Editora(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=100)
+    ano_publicacao = models.IntegerField()
+    genero=models.ForeignKey(Genero, on_delete=models.CASCADE)
+    editora=models.ForeignKey(Editora, on_delete=models.CASCADE)
+   # autor=models.ForeignKey(Autor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
+
+
+
 class Cliente(models.Model):
     nome = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
